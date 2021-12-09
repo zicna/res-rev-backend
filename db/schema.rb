ad1dac1b5fae7908660c000a2dfe2c68c1a3e0bc
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-12-09 10:58:00 -0600

    add seed data; create, igrate and seed the db

---
+++ db/schema.rb
@@ -0,0 +1,37 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_12_09_162745) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "restaurants", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
+    t.string "res_type"
+    t.string "image"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.bigint "restaurant_id", null: false
+    t.text "content"
+    t.integer "mark"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["restaurant_id"], name: "index_reviews_on_restaurant_id"
+  end
+
+  add_foreign_key "reviews", "restaurants"
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,78 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+restaurants = [
+    {
+        name: "renaldis",
+        location: "lakeview",
+        restautant_type: "bar",
+        image: 'https://media.istockphoto.com/photos/modern-restaurant-interior-design-picture-id12111097191?k=20&m=12111097191&s=612x612&w=0&h=KiZX7NBZVCK9MlSh9BJ8hZNSJcTIMbNSSV2yusw2NmM='
+    },
+    {
+        name: 'toco',
+        location: 'wicker park',
+        restautant_type: 'casual',
+        image: 'https://www.gordonramsayrestaurants.com/assets/Uploads/_resampled/CroppedFocusedImage16001090100-100-Casual-6.jpg'
+    },
+    {
+        name: 'atlantic',
+        location: 'Edgartown',
+        restautant_type: 'fine dining',
+        image: 'https://i.ndtvimg.com/i/20110-10/restaurant_6210x7100_919998996109.jpg'
+    }
+]
+
+restaurants.map do |res|
+    Restaurant.find_or_create_by(name: res[:name]) do |r|
+        r.name = res[:name]
+        r.location = res[:location]
+        r.res_type = res[:res_type]
+        r.image = res[:image]
+    puts("restaurant #{r[:name]} was created")
+    end
+end
+
+reviews = [
+    {
+        restaurant_id: 1,
+        mark: 7,
+        content: "Good food, not very clean" 
+    },
+    {
+        restaurant_id: 2,
+        mark: 9,
+        content: "Good food, service could be better" 
+    },
+    {
+        restaurant_id: 7,
+        mark: 10,
+        content: "Great food!" 
+    },
+    {
+        restaurant_id: 1,
+        mark: 1,
+        content: "Terrible experience" 
+    },
+    {
+        restaurant_id: 2,
+        mark: 10,
+        content: "Excellent overall" 
+    },
+    {
+        restaurant_id: 7,
+        mark: 7,
+        content: "so and so, for the money they should do better"
+    }
+    
+]
+reviews.map do |review|
+    Review.find_or_create_by(restaurant_id: review[:restaurant_id], content: review[:content], mark: review[:mark]) do |r|
+        r.restaurant_id = review[:restaurant_id]
+        r.content = review[:content]
+        r.mark = review[:mark]
+        puts ("review for restorant  ")
+    end
+end
+
+#{Restaurant.find_by(id: review[:restaurant_id]).name}
+
