25790587a7ccc030a450c729ecaca227b25f7899
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-12-14 15:27:04 -0600

    roundup rating contitionaly

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,7 +2,11 @@ class Restaurant < ApplicationRecord
     has_many :reviews, dependent: :destroy
 
     def rating
-        self.reviews.average(:mark).round(1)
+        if self.reviews.length > 0
+            self.reviews.average(:mark).round(1)
+        else
+            0
+        end
     end
 
     def review_count
