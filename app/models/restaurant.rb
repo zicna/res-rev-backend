2d1482a88c80ebdfe030134f5f339ac5d6ac1ebd
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-12-14 12:13:28 -0600

    round reating method in Restaurant model to one decimal place

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,7 +2,7 @@ class Restaurant < ApplicationRecord
     has_many :reviews, dependent: :destroy
 
     def rating
-        self.reviews.average(:mark)
+        self.reviews.average(:mark).round(1)
     end
 
     def review_count
