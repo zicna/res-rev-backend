2b2e04f181eb37991d1d1d1c96c7d1b9c1e69454
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-12-14 10:02:15 -0600

    change migration for default value of image attribute

--- db/migrate/20211212160931_change_column_default.rb
+++ db/migrate/20211212160931_change_column_default.rb
@@ -1,5 +1,6 @@
 class ChangeColumnDefault < ActiveRecord::Migration[6.1]
   def change
-    change_column :restaurants, :image, :string, :default => "https://images.pexels.com/photos/6267/menu-restaurant-vintage-table.jpg?auto=compress&cs=tinysrgb&dpr=1&w=400"
+    # change_column :restaurants, :image, :string, :default => "https://images.pexels.com/photos/6267/menu-restaurant-vintage-table.jpg?auto=compress&cs=tinysrgb&dpr=1&w=400"
+    change_column_default :restaurants, :image, "https://images.pexels.com/photos/6267/menu-restaurant-vintage-table.jpg?auto=compress&cs=tinysrgb&dpr=1&w=400"
   end
 end
